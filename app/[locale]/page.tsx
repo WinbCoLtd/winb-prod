38b22028bac3ee2b56d299b5792f2b64b7d880f5
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2025-01-10 17:35:10 +0530

    feat: internationslization embedded

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,14 @@
 // import Image from "next/image";
 
 import Buttons from "@/components/common/Buttons";
+import Navbar from "@/components/Navbar";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
   const t = useTranslations("HomePage")
   return (
     <main>
+      <Navbar />
       <h1>
         {t("title")}
       </h1>
