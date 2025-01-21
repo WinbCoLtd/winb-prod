e83bb020e1f4f56d685d58397eb10adcef20770a
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2025-01-21 17:35:58 +0530

    chore: padding issue in hero section

--- components/home/HerosSection.tsx
+++ components/home/HerosSection.tsx
@@ -58,7 +58,7 @@ function HerosSection() {
   }, []);
 
   return (
-    <div className="relative w-full min-h-screen flex flex-col md:px-32 xl:px-56 mx-auto bg-hero-image pb-10 bg-cover object-center bg-center aspect-video">
+    <div className="relative w-full min-h-screen flex flex-col md:px-12 xl:px-56 mx-auto bg-hero-image pb-10 bg-cover object-center bg-center aspect-video">
       <Navbar />
       <div className="flex flex-col justify-center py-10 lg:py-56 items-center h-full px-4">
         <h1 className="font-bold text-3xl sm:text-5xl lg:text-4xl md:text-6xl text-white text-center mb-10 lg:mb-20">
