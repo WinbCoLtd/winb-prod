378dc13a509a6d29f1e56b35e4ed9501c37a8c6f
Tharushi-Nethmini <it22083296@my.sliit.lk>
2025-01-21 19:34:57 +0530

    feat:ui:login

---
+++ app/[locale]/auth/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+
+const Login = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string>('');
+   
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <div className="w-full max-w-md p-6 bg-white border border-[#c3d173] rounded-lg shadow-md text-center">
+      <Image
+        src="/home/logo.png"
+        alt="Logo"
+        width={96} 
+        height={96} 
+        className="w-24 h-auto mx-auto mb-6"
+        />
+        <h2 className="text-2xl font-bold mb-6 text-gray-800">Login</h2>
+
+        {error && <p className="text-red-600 text-sm mb-4">{error}</p>}
+
+        <form  className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-left font-medium mb-1">Email:</label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Enter your email"
+              className="px-3 py-2 border  rounded-md focus:outline-none focus:ring-2 "
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-left font-medium mb-1">Password:</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Enter your password"
+              className="px-3 py-2 border  rounded-md focus:outline-none focus:ring-2"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-3/5 mx-auto px-4 py-2 bg-[#cce449] text-white font-semibold rounded-md hover:bg-[#96a830] transition"
+          >
+            Login
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-600">
+          Don’t have an account? <a href="/register" className="text-[#617bc2] hover:text-[#3c5ebb] font-medium">Register Here</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- app/[locale]/companyProfile/page.tsx
+++ app/[locale]/companyProfile/page.tsx
@@ -3,37 +3,61 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div className="relative w-full flex flex-col max-w-[1166px] mx-auto">
-      <div className="h-[127px] bg-winb-ash text-white mt-[23px] sm:mt-[12px]">Navbar</div>
+    <div className="relative w-full flex flex-col max-w-[1166px] mx-auto px-4 py-2">
+      <div className="h-[127px] bg-winb-ash text-white mt-[23px] sm:mt-[12px]">
+        Navbar
+      </div>
 
       <div
-        className="relative w-full flex flex-col bg-winb-blue mt-[72px] sm:mt-[32px] p-4 sm:p-2"
+        className="relative w-full flex flex-col bg-winb-blue mt-16 sm:mt-8 sm:p-2   rounded-md"
         style={{
           backgroundImage: "url(/companyProfile/profile.png)",
-          backgroundSize: "cover", 
-          backgroundPosition: "center", 
-          backgroundRepeat: "no-repeat", 
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
         }}
       >
-        <h1 className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-[65px] text-black font-semibold text-[36px] lg:text-[36px] md:text-[32px] sm:text-[28px] xs:text-[24px] xxs:text-[24px] leading-tight text-center">
+        <h1 className=" mt-[65px] text-black font-semibold text-[36px] lg:text-[36px] md:text-[32px] sm:text-[28px] xs:text-[24px] xxs:text-[24px] leading-tight text-center">
           Company Profile
         </h1>
 
         <div className="mt-[212px] sm:mt-[122px] xs:mt-[100px] xxs:mt-[120px] px-10 sm:px-4 xs:px-2 xxs:px-1 text-justify">
           <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
-          WIN-B Co. Ltd., founded in 2024, specializes in exporting Japanese used cars. We offer a diverse selection of vehicles from all major Japanese manufacturers, including Toyota, Honda, Nissan, Mitsubishi, Mazda, Subaru, Isuzu, Suzuki, Daihatsu, and more, covering a wide range of models. Purchasing a used car online from Japan can be challenging, but we simplify the process to make it seamless and hassle-free for you.
+            WIN-B Co. Ltd., founded in 2024, specializes in exporting Japanese
+            used cars. We offer a diverse selection of vehicles from all major
+            Japanese manufacturers, including Toyota, Honda, Nissan, Mitsubishi,
+            Mazda, Subaru, Isuzu, Suzuki, Daihatsu, and more, covering a wide
+            range of models. Purchasing a used car online from Japan can be
+            challenging, but we simplify the process to make it seamless and
+            hassle-free for you.
           </p>
           <br />
           <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
-          Recognizing the growing demand and unique requirements of the international market, we founded this organization after conducting extensive research. This involved addressing challenges from the ground up, both domestically and globally, and understanding the regulations and needs of customers in different countries. Our mission is to simplify the car-buying experience while adhering to all necessary systems and standards.
+            Recognizing the growing demand and unique requirements of the
+            international market, we founded this organization after conducting
+            extensive research. This involved addressing challenges from the
+            ground up, both domestically and globally, and understanding the
+            regulations and needs of customers in different countries. Our
+            mission is to simplify the car-buying experience while adhering to
+            all necessary systems and standards.
           </p>
           <br />
           <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
-          With access to over 230,000 vehicles auctioned weekly across Japan, you can easily find the car that fits your preferences and budget. We are committed to helping you secure the best deal, ensuring you buy a car you are completely satisfied with, all at or below your preferred price. This customer-focused approach has contributed to our rapid global growth and a loyal customer base worldwide.
+            With access to over 230,000 vehicles auctioned weekly across Japan,
+            you can easily find the car that fits your preferences and budget.
+            We are committed to helping you secure the best deal, ensuring you
+            buy a car you are completely satisfied with, all at or below your
+            preferred price. This customer-focused approach has contributed to
+            our rapid global growth and a loyal customer base worldwide.
           </p>
           <br />
           <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
-          We deeply value your interest in our organization. For any inquiries, questions, feedback, or suggestions about our services, vehicle quality, or company, please don’t hesitate to reach out. Your insights are invaluable in helping us enhance the quality of our offerings to better serve your needs. Simply fill out the contact form below to share your thoughts.
+            We deeply value your interest in our organization. For any
+            inquiries, questions, feedback, or suggestions about our services,
+            vehicle quality, or company, please don’t hesitate to reach out.
+            Your insights are invaluable in helping us enhance the quality of
+            our offerings to better serve your needs. Simply fill out the
+            contact form below to share your thoughts.
           </p>
           <br />
           <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
@@ -45,7 +69,12 @@ const Profile = () => {
           </p>
           <br />
           <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
-            If you have any inquiries, questions, comments, or suggestions regarding our services, vehicle quality, company, etc., please feel free to contact us. Your opinions, suggestions, and comments will be our assets to accelerate and improve the quality of our services as per your interests and needs. Just fill in the contact form below to send us your feedback. Thank you very much,
+            If you have any inquiries, questions, comments, or suggestions
+            regarding our services, vehicle quality, company, etc., please feel
+            free to contact us. Your opinions, suggestions, and comments will be
+            our assets to accelerate and improve the quality of our services as
+            per your interests and needs. Just fill in the contact form below to
+            send us your feedback. Thank you very much,
           </p>
           <br />
           <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-bold">
@@ -53,17 +82,8 @@ const Profile = () => {
           </p>
         </div>
       </div>
-    
     </div>
-   
   );
 };
 
 export default Profile;
-
-
-
-
-
-
-
