0b07387551636428fdf89d95727a46b460811929
Tharushi-Nethmini <it22083296@my.sliit.lk>
2025-01-22 12:29:17 +0530

    feat:ui:companyprofile

--- app/[locale]/companyProfile/page.tsx
+++ app/[locale]/companyProfile/page.tsx
@@ -17,12 +17,12 @@ const Profile = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <h1 className=" mt-[65px] text-black font-semibold text-[36px] lg:text-[36px] md:text-[32px] sm:text-[28px] xs:text-[24px] xxs:text-[24px] leading-tight text-center">
+        <h1 className=" mt-[65px] text-black font-semibold  lg:text-[36px] md:text-[32px] sm:text-[28px] xs:text-[24px] xxs:text-[24px] leading-tight text-center">
           Company Profile
         </h1>
 
-        <div className="mt-[212px] sm:mt-[122px] xs:mt-[100px] xxs:mt-[120px] px-10 sm:px-4 xs:px-2 xxs:px-1 text-justify">
-          <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
+        <div className="mt-[200px] sm:mt-[80px] xs:mt-[60px] xxs:mt-[50px] px-10 sm:px-4 xs:px-3 xxs:px-3 text-justify">
+          <p className="lg:text-[18px] sm:text-[14px] xs:text-[14px] xxs:text-[14px] font-semibold">
             WIN-B Co. Ltd., founded in 2024, specializes in exporting Japanese
             used cars. We offer a diverse selection of vehicles from all major
             Japanese manufacturers, including Toyota, Honda, Nissan, Mitsubishi,
@@ -32,7 +32,7 @@ const Profile = () => {
             hassle-free for you.
           </p>
           <br />
-          <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
+          <p className="lg:text-[18px] sm:text-[14px] xs:text-[14px] xxs:text-[14px] font-semibold">
             Recognizing the growing demand and unique requirements of the
             international market, we founded this organization after conducting
             extensive research. This involved addressing challenges from the
@@ -42,7 +42,7 @@ const Profile = () => {
             all necessary systems and standards.
           </p>
           <br />
-          <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
+          <p className="lg:text-[18px] sm:text-[14px] xs:text-[14px] xxs:text-[14px] font-semibold">
             With access to over 230,000 vehicles auctioned weekly across Japan,
             you can easily find the car that fits your preferences and budget.
             We are committed to helping you secure the best deal, ensuring you
@@ -51,7 +51,7 @@ const Profile = () => {
             our rapid global growth and a loyal customer base worldwide.
           </p>
           <br />
-          <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
+          <p className="lg:text-[18px] sm:text-[14px] xs:text-[14px] xxs:text-[14px] font-semibold">
             We deeply value your interest in our organization. For any
             inquiries, questions, feedback, or suggestions about our services,
             vehicle quality, or company, please don’t hesitate to reach out.
@@ -60,15 +60,15 @@ const Profile = () => {
             contact form below to share your thoughts.
           </p>
           <br />
-          <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
+          <p className="lg:text-[18px] sm:text-[14px] xs:text-[14px] xxs:text-[14px] font-semibold">
             Please feel free to contact us, we look forward to hearing from you.
           </p>
           <br />
-          <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-bold">
+          <p className="lg:text-[18px] sm:text-[14px] xs:text-[14px] xxs:text-[14px] font-bold">
             Dear Valued Customer,
           </p>
           <br />
-          <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-semibold">
+          <p className="lg:text-[18px] sm:text-[14px] xs:text-[14px] xxs:text-[14px] font-semibold">
             If you have any inquiries, questions, comments, or suggestions
             regarding our services, vehicle quality, company, etc., please feel
             free to contact us. Your opinions, suggestions, and comments will be
@@ -77,7 +77,7 @@ const Profile = () => {
             send us your feedback. Thank you very much,
           </p>
           <br />
-          <p className="lg:text-[20px] sm:text-[16px] xs:text-[14px] xxs:text-[14px] font-bold">
+          <p className="lg:text-[18px] sm:text-[14px] xs:text-[14px] xxs:text-[14px] font-bold mb-4">
             Minami Naori
           </p>
         </div>
